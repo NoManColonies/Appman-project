b81437edebc5f07848b7b56c98e8f9a5fb1fd258
Geng <katsunari0908@gmail.com>
2020-08-05 09:40:30 +0700

    Added login and register file(.edge)

--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,37 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use("Route");
 
-Route.on('/').render('welcome')
+Route.on("/").render("home");
+// Route.on("/register").render("register");
+
+Route.get("/login", (context) => {
+  const { view, request, response } = context;
+
+  const name = "Geng";
+  const age = 19;
+  const array = ["1", "2", "3", "4"];
+  const obj = {
+    item1: "1",
+    item2: "2",
+    item3: "3",
+  };
+
+  return view.render("login", { name, age, array, obj });
+});
+
+Route.get("/register", (context) => {
+  const { view, request, response } = context;
+
+  const name = "register";
+  const age = 20;
+  const array = ["5", "6", "7"];
+  const obj = {
+    item1: "4",
+    item2: "5",
+    item3: "6",
+  };
+
+  return view.render("register", { name, age, array, obj });
+});
