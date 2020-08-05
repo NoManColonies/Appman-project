5665f5e096ca3ecdfc6e560ad6633b72369ba1b5
Geng <katsunari0908@gmail.com>
2020-08-05 10:45:26 +0700

    Added new AuthController file

--- start/routes.js
+++ start/routes.js
@@ -19,32 +19,6 @@ const Route = use("Route");
 Route.on("/").render("home");
 // Route.on("/register").render("register");
 
-Route.get("/login", (context) => {
-  const { view, request, response } = context;
+Route.get("/login", "AuthController.login");
 
-  const name = "Geng";
-  const age = 19;
-  const array = ["1", "2", "3", "4"];
-  const obj = {
-    item1: "1",
-    item2: "2",
-    item3: "3",
-  };
-
-  return view.render("login", { name, age, array, obj });
-});
-
-Route.get("/register", (context) => {
-  const { view, request, response } = context;
-
-  const name = "register";
-  const age = 20;
-  const array = ["5", "6", "7"];
-  const obj = {
-    item1: "4",
-    item2: "5",
-    item3: "6",
-  };
-
-  return view.render("register", { name, age, array, obj });
-});
+Route.get("/register", "AuthController.register");
