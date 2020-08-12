b721af7295a23b5a91800c2062025ccfd0b424f0
chubbytoey <toey.sasikrang@gmail.com>
2020-08-12 21:25:04 +0700

    add rating system

--- public/js/detail.js
+++ public/js/detail.js
@@ -1,14 +1,32 @@
 "use strict";
-const star = document.querySelector(".add-new-rate-star-box").children;
-for (let i = 0; i < star.length; i++) {
-    star[i].addEventListener("mouseover",function () {
-        for (let j = 0; j < array.length; j++) {
-            star[j].classList.remove("fa-star")
-            star[j].classList.add("fa-star-o")
+const stars = document.querySelector(".add-new-ratestar-box").children;
+const ratingValue = document.querySelector("#ratestarvalue");
+let index;
+for (let i = 0; i < stars.length; i++) {
+    // console.log(stars)
+    stars[i].addEventListener("mouseover",function () {
+        for (let j=0; j<stars.length; j++) {
+            stars[j].classList.remove("checked");
+            stars[j].classList.add("unchecked");
         }
-        for (let j = 0; j < array.length; j++) {
-            star[j].classList.remove("fa-star-0")
-            star[j].classList.add("fa-star")
+        for (let j  = 0; j<=i; j++) {
+            stars[j].classList.remove("unchecked");
+            stars[j].classList.add("checked");
+        }
+    })
+    stars[i].addEventListener("click",function() {
+        ratingValue.value =i+1;
+        index=i+1;
+    })
+    stars[i].addEventListener("mouseout",function () {
+        console.log(index)
+        for (let j=0; j<stars.length; j++) {
+            stars[j].classList.remove("checked");
+            stars[j].classList.add("unchecked");
+        }
+        for (let j = 0; j < index; j++) {
+            stars[j].classList.remove("unchecked");
+            stars[j].classList.add("checked");
         }
     })
 }
